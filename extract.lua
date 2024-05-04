2acd0ceefe503377dfb8e7b35163135034b13b5b
Saithe <163782594+Saithe6@users.noreply.github.com>
2024-05-04 02:09:01 -0500

    Add files via upload

---
+++ extract.lua
@@ -0,0 +1,33 @@
+local redlib = require("libs/redlib")
+local chatbox = peripheral.wrap("left")
+
+local function dropDoor(integrator,side)
+  redlib.pulse(side,.1,.05,"~",integrator)
+end
+
+
+local function userLookup(username)
+  local users = {
+    Saithe6 = dropDoor(peripheral.wrap("top"),"top")
+  }
+  if users[username] == nil then return false end
+  return users[username]
+end
+
+
+local function readChat()
+  local event,username,message,uuid,hidden =  os.pullEvent("chat")
+  if hidden and message == "e" and userLookup(username) then return userLookup(username) end
+  return false
+end
+
+
+local function main()
+  while true do
+    local rSide = readChat()
+    if rSide ~= false then
+      redlib.pulse()
+    end
+  end
+end
+main()
